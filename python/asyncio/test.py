959429e4fdc3bb483259548fd09cd45907c48b15
amackillop <austin.mackillop@gmail.com>
2019-09-08 18:49:03 -0400

    Fix syntax error

--- python/asyncio/test.py
+++ python/asyncio/test.py
@@ -8,9 +8,9 @@ def test_take(some_list):
     assert some_list[0] not in new_list
     assert all([x in new_list for x in some_list[1:]])
 
-@given(st.functions(like=lambda x, y: True, returns=st.booleans(), st.booleans())
+@given(st.functions(like=lambda x, y: True, returns=st.booleans()), st.booleans())
 def test_partition(predicate, boolean):
-    
+    pass    
 
 if __name__ == '__main__':
     test_take()
