830d2d2f40191d81a0d70ec81a9171acd9c76cb4
amackillop <austin.mackillop@gmail.com>
2019-09-08 18:39:11 -0400

    hypothesis testing

--- python/asyncio/helpers.py
+++ python/asyncio/helpers.py
@@ -9,6 +9,7 @@ from collections import deque
 import itertools
 import aiohttp
 import contextlib
+import collections
 
 from my_types import *
 
@@ -43,13 +44,15 @@ async def download_image(url: str) -> str:
 
 
 # Functional Programming FTW
-def take(n, iterable):
-    "Return first n items of the iterable as a list"
-    return list(islice(iterable, n))
+# def take(n, iterable):
+#     "Return first n items of the iterable as a list"
+#     return list(islice(iterable, n))
 
-def tail(iterable):
+def tail(iterable: Iterable) -> Iterable:
     "Return an iterator over the last n items"
-    return iterable[1:]
+    deq = collections.deque(iterable)
+    deq.popleft()
+    return deq
 
 def partition(predicate: Callable[[T], bool] , iterable: Iterable[T]) -> Tuple[Iterator[T], Iterator[T]]:
     'Use a predicate to partition entries into false entries and true entries'
